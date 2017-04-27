ac2b1850c8fd2b967a4349fbb340f3a53c1f91f8
valentin <valentin-42@mail.ru>
2017-04-27 21:28:53 +0300

    реализован менеджер. не тестировался

--- VeeamSoftware_test/GZipManager.cs
+++ VeeamSoftware_test/GZipManager.cs
@@ -10,69 +10,57 @@ namespace VeeamSoftware_test.Gzip
 {
     public interface IGZipManager
     {
-        void Compress(string input, string output);
-        void Decompress(string input, string output);
+        void Execute(string sourceFile, string resultFile);
     }
 
-    public class GZipManager : IGZipManager
+    public abstract class GZipManager : IGZipManager
     {
-        public void  Compress(string input, string output)
-        {
+        private const string Compress = "compress";
+        private const string Decompress = "decompress";
 
-            using (FileStream outFile = new FileStream(output, FileMode.Create, FileAccess.Write, FileShare.None))
+        public static IGZipManager create(string act)
+        {
+            if (act.ToLower().Equals(Compress))
+                return new GZipManagerCompress();
+
+            if (act.ToLower().Equals(Decompress))
+                return new GZipManagerDecompress();
+
+            return null;
+        }
+        public abstract void Execute(string sourceFile, string resultFile);
+    }
+
+    public class GZipManagerCompress : GZipManager
+    {
+        public override void Execute(string sourceFile, string resultFile)
+        {
+            using (var sourceStream = new FileStream(sourceFile,FileMode.Open,FileAccess.Read,FileShare.Read))
             {
-                using (GZipStream gZipStream = new GZipStream(outFile, CompressionMode.Compress))
+                using (var outStream = new FileStream(resultFile, FileMode.Open, FileAccess.Read, FileShare.Read))
                 {
-                    string dir = "";
-                    GzipDriver.ZipStream = gZipStream;
-                    List<Thread> threads = new List<Thread>();
-                    foreach (string sFilePath in getPathFiles(input, out dir))
+                    using (var gZipStream = new GZipStream(outStream, CompressionMode.Compress))
                     {
-                        GzipDriver driver =new GzipDriver();
-                        driver.CompressDir = dir;
-                        driver.CompressFileName = sFilePath;
-                        //driver.ZipStream = gZipStream;
-                        threads.Add(new Thread(driver.CompressFile));
+                        GzipDriver.ModificationOfData(sourceStream, gZipStream);
                     }
-                    foreach (Thread thred in threads)
-                        thred.Start();
                 }
             }
         }
-
-        public void  Decompress(string input, string output)
+    }
+    public class GZipManagerDecompress : GZipManager
+    {
+        public override void Execute(string sourceFile, string resultFile)
         {
-            using (FileStream inFile = new FileStream(input, FileMode.Open, FileAccess.Read, FileShare.None))
+            using (var sourceStream = new FileStream(sourceFile, FileMode.Open, FileAccess.Read, FileShare.Read))
             {
-                using (GZipStream zipStream = new GZipStream(inFile, CompressionMode.Decompress, true))
+                using (var gZipStream = new GZipStream(sourceStream, CompressionMode.Decompress))
                 {
-                    //while (GzipDriver.DecompressFile(output, zipStream));
+                    using (var outStream = new FileStream(resultFile,FileMode.Create,FileAccess.Write,FileShare.None))
+                    {
+                        GzipDriver.ModificationOfData(sourceStream, outStream);
+                    }
                 }
             }
-        }
-
-        private List<string> getPathFiles(string inputDir, out string sDir)
-        {
-
-            if (File.Exists(inputDir))
-            {
-                sDir = Path.GetDirectoryName(inputDir);
-                return new List<string> {Path.GetFileName(inputDir)};
-            }
-
-            List<string> result = new List<string>();
-
-            string[] sFiles = Directory.GetFiles(inputDir, "*.*", SearchOption.AllDirectories);
-            int iDirLen = inputDir.EndsWith(Path.DirectorySeparatorChar.ToString())
-                ? inputDir.Length
-                : inputDir.Length + 1;
-            foreach (string sFilePath in sFiles)
-            {
-                result.Add(sFilePath.Substring(iDirLen));
-            }
-            sDir = inputDir;
-
-            return result;
         }
     }
 }
