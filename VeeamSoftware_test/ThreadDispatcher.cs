12401ac6318b2d7b39171d1401e34fb70e2cdd10
valentin <valentin-42@mail.ru>
2017-05-09 21:00:55 +0300

    реализован диспечер

---
+++ VeeamSoftware_test/ThreadDispatcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace VeeamSoftware_test
+{
+   public class ThreadDispatcher
+   {
+       private readonly Semaphore semaphore;
+
+       public ThreadDispatcher(int countThreads)
+       {
+           semaphore = new Semaphore(countThreads,countThreads);
+       }
+
+       public void Start(Action threadAction)
+       {
+           semaphore.WaitOne();
+           Thread thread = new Thread(ExceuteThread);
+            thread.Start(threadAction);
+       }
+
+       private void ExceuteThread(object act)
+       {
+           
+           if (act is Action)
+               (act as Action)();
+
+            semaphore.Release();
+        }
+   }
+}
