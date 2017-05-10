b0db1c040dc76c245bc854e323f8d3046148d5bd
valentin <valentin-42@mail.ru>
2017-05-11 00:02:56 +0300

    реализован основной карказ

--- VeeamSoftware_test/GzipDriver.cs
+++ VeeamSoftware_test/GzipDriver.cs
@@ -17,18 +17,17 @@ namespace VeeamSoftware_test.Gzip
     public abstract class GzipDriver : IGzipDriver
     {
         protected const long BlockSize = 10*1024*1024;
-        protected const int MaxQueueSize = 10;
-
         private readonly Thread _sourceThread;
         private readonly Thread _outputThread;
 
         protected readonly ThreadDispatcher _threadDispatcher;
         protected QueueOrder<byte[]> _bufferQueue = new QueueOrder<byte[]>();
-        protected Semaphore _bufferQueueSemaphore = new Semaphore(0, Int32.MaxValue);
         protected List<Exception> _exceptions = new List<Exception>();
 
         protected string _soutceFilePath;
         protected string _outputFilePath;
+
+        private static readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(false);
 
         protected GzipDriver()
         {
@@ -39,13 +38,56 @@ namespace VeeamSoftware_test.Gzip
         }
         public void Execute(string inputPath, string outputPath)
         {
-            
+            _soutceFilePath = inputPath;
+            _outputFilePath = outputPath;
+
+            _sourceThread.Start();
+            _outputThread.Start();
+            _autoResetEvent.WaitOne();
+        }
+        public List<Exception> Exceptions
+        {
+            get { return _exceptions; }
         }
         protected abstract void ReadStream();
 
         private void WriteStream()
         {
-            
+            try
+            {
+                using (
+                    FileStream outputStream = new FileStream(_outputFilePath, FileMode.Create, FileAccess.Write,
+                        FileShare.Read))
+                {
+                    while (_sourceThread.IsAlive || _bufferQueue.Size > 0 || !_threadDispatcher.isEmpty)
+                    {
+                        if (isBreak)
+                            break;
+
+                        byte[] buffer;
+                        if (_bufferQueue.TryGetValue(out buffer))
+                        {
+
+                            outputStream.Write(buffer, 0, buffer.Length);
+                            outputStream.Flush();
+                            GC.Collect();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                _exceptions.Add(ex);
+            }
+            finally
+            {
+                _autoResetEvent.Set();
+            }
+        }
+        protected bool isBreak
+        {
+            get { return _exceptions.Count != 0; }
         }
     }
 }
