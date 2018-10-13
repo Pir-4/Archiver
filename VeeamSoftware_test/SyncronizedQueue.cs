75b449667cff963f1052bddd9e58ca7154d50f33
valentin <valentin-42@mail.ru>
2018-10-13 22:27:17 +0300

    change algoritm in Queue

--- VeeamSoftware_test/SyncronizedQueue.cs
+++ VeeamSoftware_test/SyncronizedQueue.cs
@@ -6,126 +6,70 @@ using System.Threading;
 
 namespace VeeamSoftware_test
 {
-    /// <summary>
-    /// Упорядоченная очередь.
-    /// Для каждого эллемента указывается порядковый номер, сотоящий из основного порядка и подпорядка (если блок разбит на части)
-    /// </summary>
-    /// <typeparam name="T">Тип элементов в очереди</typeparam>
-    public class SyncronizedQueue<T>
+    internal class SyncronizedQueue<T> where T: class 
     {
-        private Dictionary<QueuOrder, T> _queueDictionary = new Dictionary<QueuOrder, T>();
-        private Dictionary<int, int> _subOrderLimits = new Dictionary<int, int>();
-        private int _currentOrder;
-        private int _currentSubOrder;
+        private class Node
+        {
+            public Node Next;
+            public long Id;
+            public T Data;
+        }
 
-        private Semaphore _semaphoreEnqueue;
-
-        AutoResetEvent _addEvent = new AutoResetEvent(false);
+        private Node _head;
+        private int _count;
+        private long _expected;
 
         public SyncronizedQueue(int maxSize = 15)
         {
-            _semaphoreEnqueue = new Semaphore(maxSize, maxSize);
+            MaxSize = maxSize;
         }
-        /// <summary>
-        /// Добавление эелмента в очередь
-        /// </summary>
-        /// <param name="order">Основной порядковый номер элемента</param>
-        /// <param name="subOrder">Второстепенный порядковый номер</param>
-        /// <param name="item">Добавляемый элемент</param>
-        /// <param name="lastSubOrder">Флаг, указывающий, что  второстепенный порядковый номер является последним
-        /// для основного порядкового номера</param>
-        public void Enqueue(int order, int subOrder, T item, bool lastSubOrder = false)
-        {
-            lock (_queueDictionary)
-            {
-                QueuOrder queuOrder = new QueuOrder(order, subOrder);
-                if (lastSubOrder)
-                    _subOrderLimits[order] = subOrder;
 
-                _queueDictionary.Add(queuOrder, item);
-                _addEvent.Set();
-            }
-        }
-        /// <summary>
-        /// Добавление элемента в очередь
-        /// </summary>
-        /// <param name="order">Порядковый номер жлемента</param>
-        /// <param name="item">Элемент</param>
-        public void Enqueue(int order, T item)
-        {
-            Enqueue(order, 0, item, true);
-        }
-        /// <summary>
-        /// получение элемента из очереди
-        /// </summary>
-        /// <param name="item">Полученный элемент</param>
-        /// <returns>true - успешное извлечение из очереди. false -элемент не найден</returns>
-        public bool TryGetValue(out T item)
-        {
-            item = default(T);
-            while (true)
-            {
-                if (!isEnd)
-                    _addEvent.WaitOne();
+        public int MaxSize { get; set; }
 
-                lock (_queueDictionary)
+        public void Enqueue(T data, long id)
+        {
+            lock (this)
+            {
+                while (Volatile.Read(ref _count) >= MaxSize || id != Interlocked.Read(ref _expected))
                 {
-                    QueuOrder queuOrder = new QueuOrder(_currentOrder, _currentSubOrder);
-                    if (_queueDictionary.TryGetValue(queuOrder, out item))
+                    Monitor.Wait(this);
+                }
+
+                var node = new Node {Data = data, Id = id};
+                if (_head == null)
+                {
+                    _head = node;
+                }
+                else
+                {
+                    var current = _head;
+                    while (current.Next != null)
                     {
-                        _queueDictionary.Remove(queuOrder);
-                        if (_subOrderLimits.ContainsKey(_currentOrder) &&
-                            _currentSubOrder == _subOrderLimits[_currentOrder])
-                        {
-                            Interlocked.Increment(ref _currentOrder);
-                            _currentSubOrder = 0;
-                        }
-                        else
-                        {
-                            Interlocked.Increment(ref _currentSubOrder);
-                        }
-                        _addEvent.Set();
-                        return true;
+                        current = current.Next;
                     }
-                    else if (isEnd && Size == 0)
-                        break;
+                    current.Next = node;
                 }
+
+                Interlocked.Increment(ref _count);
+                Interlocked.Increment(ref _expected);
+                Monitor.PulseAll(this);
             }
-            return false;
         }
 
-        public void WaitOne()
+        public bool TryGetValue(out T data, out long? id)
         {
-            _semaphoreEnqueue.WaitOne();
-        }
-
-        public void Release()
-        {
-            _semaphoreEnqueue.Release();
-        }
-        public int Size
-        {
-            get
+            lock (this)
             {
-                lock (_queueDictionary)
+                var result = Volatile.Read(ref _count) > 0;
+                data = _head?.Data;
+                id = _head?.Id;
+                if (result)
                 {
-                    return _queueDictionary.Count();
+                    _head = _head?.Next;
+                    Interlocked.Decrement(ref _count);
                 }
-            }
-        }
-        public bool isEnd { get; set; }
-
-        /// <summary>
-        /// Составной порядковый элемент в очереди
-        /// </summary>
-        private struct QueuOrder
-        {
-            public int Order;
-            public int SubOrder;
-            public QueuOrder(int order, int subOrder = 0)
-            {
-                Order = order;
-                SubOrder = subOrder;
+                Monitor.PulseAll(this);
+                return result;
             }
         }
     }
