a078e29063d14d7224a37ee176d184094eccc67c
valentin <valentin-42@mail.ru>
2018-10-13 19:49:24 +0300

    delete old code

--- VeeamSoftware_test/GZipDriver/GzipDriver.cs
+++ VeeamSoftware_test/GZipDriver/GzipDriver.cs
@@ -15,58 +15,35 @@ namespace VeeamSoftware_test.GZipDriver
     public abstract class GzipDriver : IGzipDriver
     {
         private bool _isComplited;
+        private int _maxCountReadedBlocks;
 
-        private readonly Thread _sourceThread;
-        private readonly Thread _outputThread;
+        protected string SourceFilePath;
+        protected string OutputFilePath;
 
-        protected int countTreadsOfObject;
-        protected IMyThreadPool _threadPool;
-        protected MyQueue<byte[]> BufferMyQueue = new MyQueue<byte[]>();
-
-        protected string _soutceFilePath;
-        private string _outputFilePath;
-
-        protected Stream sourceStream;
-
-        private static readonly AutoResetEvent _autoResetEvent = new AutoResetEvent(false);
 
         protected GzipDriver()
         {
-            _sourceThread = new Thread(ReadStream);
-            _outputThread = new Thread(WriteStream);
 
-            countTreadsOfObject = 1;
         }
 
         public void Execute(string inputPath, string outputPath)
         {
-            _soutceFilePath = inputPath;
-            _outputFilePath = outputPath;
-
-            _threadPool = new MyThreadPool(Environment.ProcessorCount - countTreadsOfObject);
-
-            _sourceThread.Start();
-            _outputThread.Start();
-
-            _autoResetEvent.WaitOne();
-
-            _sourceThread.Join();
-            _outputThread.Join();
+            SourceFilePath = inputPath;
+            OutputFilePath = outputPath;
         }
 
         public List<Exception> Exceptions { get; set; } = new List<Exception>();
 
         protected abstract int GetBlockLength(Stream stream);
-        protected abstract byte[] ProcessBlcok(byte[] input);
 
-        private int MaxCountReadedBlocks { get; set; }
+        protected abstract byte[] ProcessBlcok(byte[] input);
 
         private void Read()
         {
             try
             {
                 var id = 0;
-                using (var inputStream = File.OpenRead(_soutceFilePath))
+                using (var inputStream = File.OpenRead(SourceFilePath))
                 {
                     while (!_isComplited && inputStream.Position < inputStream.Length)
                     {
@@ -76,7 +53,7 @@ namespace VeeamSoftware_test.GZipDriver
                         //TODO создать очередь для чтения
                     }
                 }
-                MaxCountReadedBlocks = id;
+                _maxCountReadedBlocks = id;
             }
             catch (Exception e)
             {
@@ -112,9 +89,9 @@ namespace VeeamSoftware_test.GZipDriver
             try
             {
                 var expectedId = 0;
-                using (var outputStrem = new FileStream(_outputFilePath, FileMode.Append))
+                using (var outputStrem = new FileStream(OutputFilePath, FileMode.Append))
                 {
-                    while (!_isComplited && expectedId < MaxCountReadedBlocks)
+                    while (!_isComplited && expectedId < _maxCountReadedBlocks)
                     {
                         if (true)//TODO вставить попытка взять объект из очереди записи
                         {
